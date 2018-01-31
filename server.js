75067685638aa6a19bc0a45bda7358193bd8b82f
Andy Fry <andyfry01@gmail.com>
2018-01-31 14:29:33 -0500

    refactor: deployment refactor and tweak

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.use(compress());
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/scripts', express.static(path.join(__dirname, 'node_modules')));
+
 
 app.listen(process.env.PORT || 3000);
 
